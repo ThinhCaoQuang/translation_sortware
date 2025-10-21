b7e6a2c9765ea46ff06febe687a3b8e4a0e89833
NguyenHaPhuongTan <nguyenhaphuongtan@gmail.com>
2025-10-22 00:00:21 +0700

    Remove unnecessary files

--- main.py
+++
@@ -1,5 +0,0 @@
-import flet as ft
-from ui import main
-
-if __name__ == "__main__":
-    ft.app(target=main)
--- text_to_speech.py
+++
@@ -1,8 +0,0 @@
-def speak(text: str):
-    try:
-        import pyttsx3
-        engine = pyttsx3.init()
-        engine.say(text or "No content")
-        engine.runAndWait()
-    except Exception as ex:
-        raise RuntimeError(f"TTS error: {ex}")
