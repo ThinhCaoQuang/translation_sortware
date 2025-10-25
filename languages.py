60220674adf8b0ae3cf2a69c92346133ae53e163
maivanvangf <maivanvangf@gmail.com>
2025-10-25 23:58:55 +0700

    bổ sung ngôn ngữ Châu Á

---
+++ languages.py
@@ -0,0 +1,24 @@
+LANGUAGES = {
+    "Auto Detect": "auto",
+
+    # Châu Á
+    "Tiếng Việt": "vi",
+    "Tiếng Trung (Giản thể)": "zh",
+    "Tiếng Trung (Phồn thể)": "zh-tw",
+    "Tiếng Nhật": "ja",
+    "Tiếng Hàn": "ko",
+    "Tiếng Thái": "th",
+    "Tiếng Indonesia": "id",
+    "Tiếng Hindi": "hi",
+    "Tiếng Urdu": "ur",
+    "Tiếng Bengal (Bengali)": "bn",
+    "Tiếng Tamil": "ta",
+    "Tiếng Telugu": "te",
+    "Tiếng Gujarati": "gu",
+    "Tiếng Kannada": "kn", 
+    "Tiếng Malayalam": "ml",
+    "Tiếng Marathi": "mr",
+    "Tiếng Nepali": "ne",
+    "Tiếng Sinhala": "si",
+    "Tiếng Tagalog": "tl",
+}
